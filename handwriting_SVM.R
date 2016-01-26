300a4f259fc15f8265d6136b15377350da6f2e7d
dickm <dickm@upmc.edu>
2016-01-26 18:53:57 -0500

    Added training and evaluation

--- handwriting_SVM.R
+++ handwriting_SVM.R
@@ -1,11 +1,21 @@
+install.packages("e1071")
 library(e1071)
-ptm <- proc.time()
+
 train<-read.csv("short_prac_train.csv")
 train$label<-as.factor(train$label)
 test<-read.csv("short_prac_test.csv")
 test$label<-as.factor(test$label)
 
+
+ptm <- proc.time() # Time training.
 train.tune<-tune.svm(label~.,data=train,kernel='polynomial',degree=5)
+proc.time() - ptm
 # Polynomial kernel with 5 degrees, chosen in according to Burgess (NIPS, 1996)
 train.tune$best.model
-proc.time() - ptm
+
+##  Predict and Evaluate
+train.svm<-svm(label~.,train,cost=train.tune$best.model$cost,gamma=train.tune$best.model$gamma,coef0=train.tune$best.model$coef0,kernel='polynomial',degree=5)
+table(test$label,predict(train.svm,newdata=test[,-1]))
+sum(diag(table(test$label,predict(train.svm,newdata=test[,-1]))))/nrow(test)
+
+### Result is accurracy of 0.92.
